109b115857d0bbb5cf75ee30515cf7d041364d55
AtotheMfnK <tenfarmersandbananas@gmail.com>
2025-12-21 16:34:47 +0100

    test

--- Frontend/src/layout/Navbar.jsx
+++ Frontend/src/layout/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
+
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
+    <nav className="navbar">
       <Link to="/" style={{ marginRight: 20 }}>Home</Link>
       <Link to="/about" style={{ marginRight: 20 }}>About</Link>
       <Link to="/contact" style={{ marginRight: 20 }}>Kontakt</Link>
