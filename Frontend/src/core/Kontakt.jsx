b88a40e8e8213f3edba1ee8ecdfebf615014f27f
AtotheMfnK <tenfarmersandbananas@gmail.com>
2025-12-22 19:20:45 +0100

    erster Button funktion in Kontakt eingefügt

--- Frontend/src/core/Kontakt.jsx
+++ Frontend/src/core/Kontakt.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+
 const Kontakt = () => {
+
+  const handleClick = () => {
+  alert("Danke für deine Nachricht!");
+};
+
+
   return (
+    <>
     <div>Kontakt</div>
+    <h3>So kontaktierst Du uns</h3>
+    <p>Lass Deine Email Adresse um eine Kontakt reicher werden</p>
+    <button onClick={handleClick}>HALL LINDA HIER KLICKEN WENN ES KLICK MACHT</button>
+    </>
   )
 }
 
